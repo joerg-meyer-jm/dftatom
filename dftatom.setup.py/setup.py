c91f21700a29aef9deb1179fc0601a939d158eab
Joerg Meyer <j.meyer@chem.leidenuniv.nl>
2017-11-21 23:46:53 +0100

    modified dftatom.setup.py to build under Win32

--- dftatom.setup.py/setup.py
+++ dftatom.setup.py/setup.py
@@ -2,6 +2,14 @@
 # -*- coding: utf-8 -*-
 
 # dirty hack!
+# |+ works with gfortram homebrew build on Mac
+# |- mingw(32)py from anaconda2/pip on Windows7 (32-bit) does not print --libdir
+#    would have to parse these lines instead to extract path information for libgfortran.a :
+#     (C:\Users\IEUser\Anaconda2) C:\Users\IEUser>gfortran -v
+#     Reading specs from C:/Users/IEUser/Anaconda2/share/mingwpy/bin/../lib/gcc/i686-w64-mingw32/4.9.2/specs
+#     COLLECT_GCC=C:\Users\IEUser\Anaconda2\Scripts\..\share\mingwpy\bin\gfortran.exe
+#     COLLECT_LTO_WRAPPER=C:/Users/IEUser/Anaconda2/share/mingwpy/bin/../libexec/gcc/i686-w64-mingw32/4.9.2/lto-wrapper.exe
+#     [...]
 def get_gfortran_libdir():
 	from subprocess import Popen, PIPE
 	cmd = 'gfortran -v'
@@ -24,7 +32,7 @@ if __name__ == '__main__':
             language='c++',
             include_dirs = [numpy.get_include()],
             libraries = ["dftatom"],
-            library_dirs = ["../build/src"]
+            library_dirs = [join("..", "build", "src")]
         )
     ]
     # Makefile.manual 
@@ -35,7 +43,18 @@ if __name__ == '__main__':
             include_dirs = [numpy.get_include()],
             libraries = ["gfortran"],
             library_dirs = [get_gfortran_libdir()],
-            extra_objects = ["../src/libdftatom.a"]
+            extra_objects = [join("..", "src", "libdftatom.a")]
+        )
+    ]
+    # Makefile.manual for mingw(32)py from anaconda2/pip on Windows7 (32-bit)
+    # | assuming that gfortran has been used for building
+    dftatom_extension_manual_win32 = [
+        Extension("dftatom._dftatom_wrapper", [join("dftatom","_dftatom_wrapper.pyx")], 
+            language='c++',
+            include_dirs = [numpy.get_include()],
+            libraries = ["gfortran","quadmath"],
+            library_dirs = ["C:\Users\IEUser\Anaconda2\share\mingwpy\lib\gcc\i686-w64-mingw32\4.9.2"],
+            extra_objects = [join("..", "src", "libdftatom.a")]
         )
     ]
 
@@ -50,5 +69,5 @@ if __name__ == '__main__':
         license = "BSD",
         platforms = "(only limited by availability of Fortran 90 compilers)",
         packages = ['dftatom'],
-        ext_modules = cythonize(dftatom_extension_cmake)
+        ext_modules = cythonize(dftatom_extension_manual)
        )
