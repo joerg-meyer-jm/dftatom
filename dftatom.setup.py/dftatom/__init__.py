8a314322494005a68d63bcd3f251a29ea9811409
Joerg Meyer <j.meyer@chem.leidenuniv.nl>
2020-11-11 20:20:02 +0100

    remove dftatom.setup.py directory

--- dftatom.setup.py/dftatom/__init__.py
+++
@@ -1,4 +0,0 @@
-from _dftatom_wrapper import (getvxc, integrate_radial_poisson,
-        solve_radial_eigenproblem, ConvergeError, atom_lda, mesh_exp,
-        mesh_exp_deriv, get_Vh, integrate, atom_rlda,
-        integrate_radial_problem_outward)
